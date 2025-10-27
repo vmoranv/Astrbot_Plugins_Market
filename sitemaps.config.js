293f88a4afa2cfb3f3cd5f1e507fbd2757394d09
LI SONGSONG 🍂 <larch-c@outlook.jp>
2025-10-27 23:29:58 +0800

    Fix formatting in sitemaps.config.js

--- sitemaps.config.js
+++ sitemaps.config.js
@@ -1,6 +1,6 @@
 export default [
   'https://blog.astrbot.app/sitemap.xml',
-  'https://astrbot.app/sitemap.xml'
+  'https://astrbot.app/sitemap.xml',
   'https://astrbot.tech/sitemap.xml',
   'https://docs.astrbot.app/sitemap.xml'
 ]
